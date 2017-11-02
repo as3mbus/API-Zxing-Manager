55aaf6c2ef65c25b72c3402a4f508012c87dba06
as3mbus <asembus@gmail.com>
2017-11-03 02:06:58 +0700

    add file phone storage write

--- app/src/main/java/io/github/as3mbus/offline_qr_manager/DataManager.kt
+++ app/src/main/java/io/github/as3mbus/offline_qr_manager/DataManager.kt
@@ -1,9 +1,13 @@
 package io.github.as3mbus.offline_qr_manager
 
 import android.app.assist.AssistContent
+import android.os.Environment
 import android.support.v7.app.AppCompatActivity
 import java.io.File
 import java.io.FileOutputStream
+import android.os.Environment.MEDIA_MOUNTED_READ_ONLY
+import android.os.Environment.MEDIA_MOUNTED
+import android.view.ViewParent
 
 
 /**
@@ -13,20 +17,47 @@ import java.io.FileOutputStream
 class DataManager {
     companion object {
         fun writeInternal(activity :AppCompatActivity, FILENAME : String, content: String){
-            val path = activity.applicationContext.getFilesDir()
-            val FILENAME = "hello_file"
-            val string = "hello world!"
+            val path = activity.applicationContext.filesDir.path
             val saveTarget= File(path, FILENAME)
             FileOutputStream(saveTarget).use {
-                it.write(string.toByteArray())
+                it.write(content.toByteArray())
             }
         }
-        fun readInternal(activity :AppCompatActivity){
-            val path = activity.applicationContext.getFilesDir()
-            val FILENAME = "hello_file"
+        fun readInternal(activity :AppCompatActivity, FILENAME : String){
+            val path = activity.applicationContext.filesDir.path
             val saveTarget = File(path, FILENAME)
             println(saveTarget.readText())
         }
+
+        /* Checks if external storage is available for read and write */
+        fun isExternalStorageWritable(): Boolean {
+            val state = Environment.getExternalStorageState()
+            return if (Environment.MEDIA_MOUNTED.equals(state)) {
+                true
+            } else false
+        }
+
+        /* Checks if external storage is available to at least read */
+        fun isExternalStorageReadable(): Boolean {
+            val state = Environment.getExternalStorageState()
+            return if (Environment.MEDIA_MOUNTED.equals(state) || Environment.MEDIA_MOUNTED_READ_ONLY.equals(state)) {
+                true
+            } else false
+        }
+        fun writePhone(activity: AppCompatActivity?,FILENAME: String, parentFolder: String?,content: ByteArray)
+        {
+            val path = activity?.applicationContext?.getExternalFilesDir(parentFolder)?.path
+            val saveTarget= File(path, FILENAME)
+            FileOutputStream(saveTarget).use {
+                it.write(content)}
+        }
+        fun readPhone(activity :AppCompatActivity, FILENAME : String,parentFolder: String?)
+        {
+            val path = activity.applicationContext.getExternalFilesDir(parentFolder).path
+            val saveTarget = File(path, FILENAME)
+            println(saveTarget.readText())
+        }
+
 
     }
 }
