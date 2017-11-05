e03177824a4b77e6c4baacc0516d8f3587404639
as3mbus <asembus@gmail.com>
2017-11-05 22:09:30 +0700

    add login confirmation function to go into main activity

--- app/src/main/java/io/github/as3mbus/offline_qr_manager/OutletConfirmActivity.kt
+++ app/src/main/java/io/github/as3mbus/offline_qr_manager/OutletConfirmActivity.kt
@@ -1,7 +1,10 @@
 package io.github.as3mbus.offline_qr_manager
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.content.ContextCompat
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_outlet_confirm.*
 
 class OutletConfirmActivity : AppCompatActivity() {
@@ -12,5 +15,19 @@ class OutletConfirmActivity : AppCompatActivity() {
         val bundle = intent.extras
 
         outletMessageTextView.text = resources.getString(R.string.outlet_confirmation_message,bundle.getString("outlet"))
+        confirmButton.setOnClickListener{
+            if(checkPassword()){
+                Toast.makeText(this,"Confirmation Finished",Toast.LENGTH_SHORT).show()
+                val i = Intent(this, MainActivity::class.java)
+                startActivity(i)
+            }
+            else{
+                Toast.makeText(this,"invalid password",Toast.LENGTH_SHORT).show()
+                passwordField.setText("")
+            }
+        }
+    }
+    fun checkPassword() : Boolean{
+        return passwordField.text.toString().equals(resources.getString(R.string.password))
     }
 }
