4b4cb0b3d60bae6c737553357c4ad5bf8877316b
as3mbus <asembus@gmail.com>
2017-11-10 08:25:12 +0700

    initiating api call files and class *not tested

---
+++ app/src/main/java/io/github/as3mbus/offline_qr_manager/BackendAPI.kt
@@ -0,0 +1,13 @@
+package io.github.as3mbus.offline_qr_manager
+
+import com.loopj.android.http.AsyncHttpClient
+import com.loopj.android.http.AsyncHttpResponseHandler
+import com.loopj.android.http.RequestParams
+
+/**
+ * Created by as3mbus on 10/11/17.
+ */
+public class BackendAPI{
+
+
+}
---
+++ app/src/main/java/io/github/as3mbus/offline_qr_manager/BackendAPIRestClient.kt
@@ -0,0 +1,32 @@
+package io.github.as3mbus.offline_qr_manager
+
+import com.loopj.android.http.AsyncHttpClient
+import com.loopj.android.http.AsyncHttpResponseHandler
+import com.loopj.android.http.RequestParams
+
+/**
+ * Created by as3mbus on 09/11/17.
+ */
+public class BackendAPIRestClient{
+    companion object {
+        private val BASE_URL = "http://103.246.107.62:23000/"
+
+        private val client = AsyncHttpClient()
+
+        fun get(url: String, params: RequestParams, responseHandler: AsyncHttpResponseHandler) {
+            client.get(getAbsoluteUrl(url), params, responseHandler)
+        }
+
+        fun post(url: String, params: RequestParams, responseHandler: AsyncHttpResponseHandler) {
+            client.post(getAbsoluteUrl(url), params, responseHandler)
+        }
+        fun patch(url: String, params: RequestParams, responseHandler: AsyncHttpResponseHandler) {
+            client.patch(getAbsoluteUrl(url), params, responseHandler)
+        }
+
+        fun getAbsoluteUrl(relativeUrl: String): String {
+            return BASE_URL + relativeUrl
+        }
+    }
+
+}
