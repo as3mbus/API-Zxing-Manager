71c8aa4eef531cc6b1134078eb615a2ab8869162
as3mbus <asembus@gmail.com>
2017-11-12 12:06:37 +0700

    few import fix

--- app/src/main/java/io/github/as3mbus/QRManager/MainActivity.kt
+++ app/src/main/java/io/github/as3mbus/QRManager/MainActivity.kt
@@ -5,6 +5,7 @@ import android.content.Context
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
+import android.support.v4.content.ContextCompat
 import android.support.v7.app.AppCompatActivity
 import android.widget.Toast
 import com.beust.klaxon.Parser
