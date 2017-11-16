5c615f1c76f7b1bed41e1ed1b88a09b7b4796c97
as3mbus <asembus@gmail.com>
2017-11-16 13:45:49 +0700

    async task to generate QR Code

--- app/src/main/java/io/github/as3mbus/QRManager/RedeemActivity.kt
+++ app/src/main/java/io/github/as3mbus/QRManager/RedeemActivity.kt
@@ -1,8 +1,10 @@
 package io.github.as3mbus.QRManager
 
 import android.graphics.Bitmap
+import android.os.AsyncTask
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import com.google.zxing.WriterException
 import kotlinx.android.synthetic.main.activity_redeem.*
 
 class RedeemActivity : AppCompatActivity() {
@@ -19,16 +21,33 @@ class RedeemActivity : AppCompatActivity() {
             actionButton.text = resources.getText(R.string.redeem_button)
 
         } else {
+            LongOperation().execute(bundle.getString("outlet","ABC"))
             toolbar.setTitle(R.string.activate_title)
             actionButton.text = resources.getText(R.string.activate_button)
 
 
         }
-
-
-
-
         outletTextView.text = bundle.getString("outlet")
+
+    }
+
+    private inner class LongOperation : AsyncTask<String, Int, Bitmap>() {
+        override fun doInBackground(vararg p0: String?): Bitmap? {
+            val param =p0[0]
+            var bitmap:Bitmap?=null
+            try {
+                bitmap = QRGenerator.TextToImageEncode(param!!)
+            } catch (e: WriterException) {
+                e.printStackTrace()
+            }
+            return bitmap
+        }
+
+
+        override fun onPostExecute(result: Bitmap?) {
+            super.onPostExecute(result)
+            actionImageView.setImageBitmap(result)
+        }
 
     }
 }
