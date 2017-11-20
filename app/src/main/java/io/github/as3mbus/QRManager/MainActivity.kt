8f1d03b1d316f7699f11de561d07bfcc1138e4f6
as3mbus <asembus@gmail.com>
2017-11-20 22:15:59 +0700

    activate code tested successfully

--- app/src/main/java/io/github/as3mbus/QRManager/MainActivity.kt
+++ app/src/main/java/io/github/as3mbus/QRManager/MainActivity.kt
@@ -3,9 +3,9 @@ package io.github.as3mbus.QRManager
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.os.Debug
 import android.support.v7.app.AppCompatActivity
 import android.widget.Toast
-import com.beust.klaxon.Parser
 import com.google.zxing.integration.android.IntentIntegrator
 import com.google.zxing.integration.android.IntentResult
 import com.loopj.android.http.JsonHttpResponseHandler
@@ -17,95 +17,23 @@ import org.json.JSONObject
 class MainActivity : AppCompatActivity() {
     val PREFS_NAME = "OutletPrefs"
     private var context: Context? = null
-    private var redeemActivate = false
+    private var activateRedeem = true
     var scanResultVar: String? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         context = this.applicationContext
 
-        val parser = Parser()
-
-//        callButton.setOnClickListener {
-
-        //            BackendAPIRestClient.getActive("abc123", object : JsonHttpResponseHandler() {
-//                override fun onSuccess(statusCode: Int, headers: Array<Header>, response: JSONObject) {
-//                    // If the response is JSONObject instead of expected JSONArray
-//
-//                    val json: JsonObject = parser.parse(response.toString()) as JsonObject
-//                    val message = json.string("message")
-//
-//                    // Do something with the response
-//                    Toast.makeText(context, message,Toast.LENGTH_SHORT).show()
-//                }
-//            })
-
-
-//            val client = AsyncHttpClient()
-//            client.get("http://api.img4me.com/?text=Testing", object : TextHttpResponseHandler() {
-//                override fun onFailure(statusCode: Int, headers: Array<out Header>?, responseString: String?, throwable: Throwable?) {
-//                    Toast.makeText(context, "connection failure", Toast.LENGTH_SHORT).show()
-//                }
-//
-//                override fun onSuccess(statusCode: Int, headers: Array<out Header>?, responseString: String?) {
-//                    val i = Intent(context, RedeemActivity::class.java)
-//
-//                    //Create the bundle
-//                    val bundle = Bundle()
-//
-//                    //Add your data to bundle
-//                    bundle.putString("outlet", responseString)
-//
-//                    //Add the bundle to the intent
-//                    i.putExtras(bundle)
-//                    i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
-//                    //Fire that second activity
-//
-//
-//                    ContextCompat.startActivity(context, i, bundle)
-//                }
-//            })
-
-
-//            BackendAPIRestClient.get("?text=Testing",null, object : TextHttpResponseHandler{
-//                override fun onSuccess(statusCode: Int, headers: Array<Header>, response: JSONObject) {
-//                    // If the response is JSONObject instead of expected JSONArray
-//
-//                    val json: JsonObject = parser.parse(response.toString()) as JsonObject
-//                    val message = json.string("message")
-//
-//                    // Do something with the response
-//                    Toast.makeText(context, message,Toast.LENGTH_SHORT).show()
-//                }
-//            })
-//            val i = Intent(this, RedeemActivity::class.java);
-//
-//            //Create the bundle
-//            val bundle = Bundle();
-//
-//            //Add your data to bundle
-//            bundle.putString("outlet", "eyooo");
-//
-//            //Add the bundle to the intent
-//            i.putExtras(bundle);
-//
-//            //Fire that second activity
-//
-//            ContextCompat.startActivity(this, i, bundle);
-
-//        }
-        scanButton.setOnClickListener {
-
+        activateButton.setOnClickListener {
             val intentIntegr = IntentIntegrator(this)
             intentIntegr.initiateScan()
-            redeemActivate = true
+            activateRedeem = true
         }
-        reportButton.setOnClickListener {
+        redeemButton.setOnClickListener {
             val intentIntegr = IntentIntegrator(this)
             intentIntegr.initiateScan()
-            redeemActivate = false
+            activateRedeem = false
         }
-
 
     }
 
@@ -118,116 +46,122 @@ class MainActivity : AppCompatActivity() {
                 Toast.makeText(this.applicationContext, "contacting server", Toast.LENGTH_SHORT).show()
                 val settings = getSharedPreferences(PREFS_NAME, 0)
                 val outletid = settings.getInt("outletid", -1)
-                if (redeemActivate) {
-                    BackendAPIRestClient(this.applicationContext).getIsRedeemed(scanResult.contents,outletid, object : JsonHttpResponseHandler() {
-                        override fun onSuccess(statusCode: Int, headers: Array<out Header>?, response: JSONObject) {
-                            super.onSuccess(statusCode, headers, response)
-
-                            val i = Intent(context, RedeemActivity::class.java)
-                            i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
-
-                            var success = false
-                            var isExpired = true
-                            var isActivated = false
-                            var isRedeemed = true
-                            var outletOrigin = -1
-                            var expiryDate = ""
-                            var outletOriginName = ""
-                            var outletPromo = ""
-                            var usedDate = ""
-
-                            try {
-                                success = response.getBoolean("success")
-                                isExpired = response.getBoolean("isExpired")
-                                isRedeemed = response.getBoolean("isRedeemed")
-                                outletPromo = response.getJSONObject("outletId")?.getString("promo")!!
-                                isActivated = response.getJSONObject("vocher")?.getBoolean("activated")!!
-                                outletOrigin = response.getJSONObject("vocher")?.getInt("outletOrigin")!!
-                                expiryDate = response.getJSONObject("vocher")?.getString("expiryDate")!!
-                                usedDate = response.getJSONObject("outletCode")?.getString("createdAt")!!
-                            } catch (e: Exception) {
-                            }
-
-                            //Create the bundle
-                            val bundle = Bundle()
-
-                            //Add your data to bundle
-                            bundle.putString("code", scanResultVar)
-                            bundle.putBoolean("redeemActivate", redeemActivate)
-                            bundle.putBoolean("success", success)
-                            bundle.putBoolean("isExpired", isExpired)
-                            bundle.putBoolean("isActivated", isActivated)
-                            bundle.putBoolean("isRedeemed", isRedeemed)
-                            bundle.putInt("outletOrigin",outletOrigin);
-                            bundle.putString("expiryDate", expiryDate)
-                            bundle.putString("outletOriginName", outletOriginName)
-                            bundle.putString("outletPromo", outletPromo)
-                            bundle.putString("usedDate", usedDate)
-
-
-
-
-                            //Add the bundle to the intent
-                            i.putExtras(bundle)
-                            //Fire that second activity
-                            startActivity(i, bundle)
-//                        Toast.makeText(context, response?.getString("status"), Toast.LENGTH_SHORT).show()
-
-                        }
-                    })
-                } else {
-                    BackendAPIRestClient(this.applicationContext).getIsActive(scanResult.contents, object : JsonHttpResponseHandler() {
-                        override fun onSuccess(statusCode: Int, headers: Array<out Header>?, response: JSONObject) {
-                            super.onSuccess(statusCode, headers, response)
-
-                            val i = Intent(context, RedeemActivity::class.java)
-                            i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
-                            var success = false
-                            var isExpired = true
-                            var isActivated = false
-                            var outletOrigin = -1
-                            var expiryDate = ""
-                            var outletName = ""
-
-                            try {
-                                success = response.getBoolean("success")
-                                isExpired = response.getBoolean("isExpired")
-                                isActivated = response.getBoolean("isActivated")
-                                outletOrigin = response.getJSONObject("vochercode")?.getInt("outletOrigin")!!
-                                expiryDate = response.getJSONObject("vochercode")?.getString("expiryDate")!!
-                                outletName = response.getJSONObject("vochercode")?.getJSONObject("Outlet")?.getString("name")!!
-                            } catch (e: Exception) {
-                            }
-                            val permission = outletOrigin == outletid
-
-                            //Create the bundle
-                            val bundle = Bundle()
-
-                            //Add your data to bundle
-                            bundle.putString("code", scanResultVar)
-                            bundle.putBoolean("redeemActivate", redeemActivate)
-                            bundle.putBoolean("success", success)
-                            bundle.putBoolean("isExpired", isExpired)
-                            bundle.putBoolean("isActivated", isActivated)
-                            bundle.putBoolean("permission", permission)
-                            bundle.putString("expiryDate", expiryDate)
-                            bundle.putString("outletName", outletName)
-
-
-                            //Add the bundle to the intent
-                            i.putExtras(bundle)
-                            //Fire that second activity
-                            startActivity(i, bundle)
-//                        Toast.makeText(context, response?.getString("status"), Toast.LENGTH_SHORT).show()
-
-                        }
-                    })
-                }
-
-
+                if (activateRedeem)
+                    activateRequest(scanResult.contents, outletid)
+                else
+                    redeemRequest(scanResult.contents,outletid)
             }
-
         } else
             Toast.makeText(this, "scan Canceled", Toast.LENGTH_SHORT).show()
+    }
+
+    private fun activateRequest(code :String, outletid: Int){
+        BackendAPIRestClient(this.applicationContext).getIsActive(code, object : JsonHttpResponseHandler() {
+            override fun onSuccess(statusCode: Int, headers: Array<out Header>?, response: JSONObject) {
+                super.onSuccess(statusCode, headers, response)
+
+                val i = Intent(context, RedeemActivity::class.java)
+                i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+                var success = false
+                var isExpired = true
+                var isActivated = false
+                var outletOrigin = -1
+                var expiryDate = ""
+                var outletName = ""
+
+                try {
+                    success = response.getBoolean("success")
+                    isExpired = response.getBoolean("isExpired")
+                    isActivated = response.getBoolean("isActivated")
+                    outletOrigin = response.getJSONObject("vochercode")?.getInt("outletOrigin")!!
+                    expiryDate = response.getJSONObject("vochercode")?.getString("expiryDate")!!
+                    outletName = response.getJSONObject("vochercode")?.getJSONObject("Outlet")?.getString("name")!!
+                } catch (e: Exception) {
+                    println("==============RETRIEVAL FAILURE===================")
+                    println(""+success+" "+isExpired+" "+isActivated+" ")
+                }
+                val permission = outletOrigin == outletid
+
+                //Create the bundle
+                val bundle = Bundle()
+
+                //Add your data to bundle
+                bundle.putString("code", scanResultVar)
+                bundle.putBoolean("activateRedeem", activateRedeem)
+                bundle.putBoolean("success", success)
+                bundle.putBoolean("isExpired", isExpired)
+                bundle.putBoolean("isActivated", isActivated)
+                bundle.putBoolean("permission", permission)
+                bundle.putString("expiryDate", expiryDate)
+                bundle.putString("outletName", outletName)
+
+
+                //Add the bundle to the intent
+                i.putExtras(bundle)
+                //Fire that second activity
+                startActivity(i, bundle)
+//                        Toast.makeText(context, response?.getString("status"), Toast.LENGTH_SHORT).show()
+
+            }
+        })
+    }
+
+    private fun redeemRequest(code:String,outletid:Int){
+        BackendAPIRestClient(this.applicationContext).getIsRedeemed(code,outletid, object : JsonHttpResponseHandler() {
+            val outletid = outletid
+            override fun onSuccess(statusCode: Int, headers: Array<out Header>?, response: JSONObject) {
+                super.onSuccess(statusCode, headers, response)
+
+                val i = Intent(context, RedeemActivity::class.java)
+                i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+
+                var success = false
+                var isExpired = true
+                var isActivated = false
+                var isRedeemed = true
+                var outletOrigin = -1
+                var expiryDate = ""
+                var outletPromo = ""
+                var usedDate = ""
+
+                try {
+                    success = response.getBoolean("success")
+                    isExpired = response.getBoolean("isExpired")
+                    isRedeemed = response.getBoolean("isRedeemed")
+                    outletPromo = response.getJSONObject("outletId")?.getString("promo")!!
+                    isActivated = response.getJSONObject("vocher")?.getBoolean("activated")!!
+                    outletOrigin = response.getJSONObject("vocher")?.getInt("outletOrigin")!!
+                    expiryDate = response.getJSONObject("vocher")?.getString("expiryDate")!!
+                    usedDate = response.getJSONObject("outletCode")?.getString("createdAt")!!
+                } catch (e: Exception) {
+                }
+
+                //Create the bundle
+                val bundle = Bundle()
+
+                //Add your data to bundle
+                bundle.putString("code", scanResultVar)
+                bundle.putInt("outletid",this.outletid)
+                bundle.putBoolean("activateRedeem", activateRedeem)
+                bundle.putBoolean("success", success)
+                bundle.putBoolean("isExpired", isExpired)
+                bundle.putBoolean("isActivated", isActivated)
+                bundle.putBoolean("isRedeemed", isRedeemed)
+                bundle.putInt("outletOrigin",outletOrigin);
+                bundle.putString("expiryDate", expiryDate)
+                bundle.putString("outletPromo", outletPromo)
+                bundle.putString("usedDate", usedDate)
+
+
+
+
+                //Add the bundle to the intent
+                i.putExtras(bundle)
+                //Fire that second activity
+                startActivity(i, bundle)
+//                        Toast.makeText(context, response?.getString("status"), Toast.LENGTH_SHORT).show()
+
+            }
+        })
     }
 }
--- app/src/main/java/io/github/as3mbus/QRManager/RedeemActivity.kt
+++ app/src/main/java/io/github/as3mbus/QRManager/RedeemActivity.kt
@@ -1,87 +1,38 @@
 package io.github.as3mbus.QRManager
 
+import android.annotation.SuppressLint
+import android.content.Context
+import android.content.Intent
 import android.graphics.Bitmap
 import android.os.AsyncTask
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.widget.Toast
 import com.google.zxing.WriterException
+import com.loopj.android.http.JsonHttpResponseHandler
+import cz.msebera.android.httpclient.Header
 import kotlinx.android.synthetic.main.activity_redeem.*
+import org.json.JSONObject
 
 class RedeemActivity : AppCompatActivity() {
+    var context :Context? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        context = this.applicationContext
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_redeem)
 
         val bundle = intent.extras
 
-        val redeemActivate = bundle.getBoolean("redeemActivate")
-        if (redeemActivate) {
-            toolbar.setTitle(R.string.redeem_title)
-            actionButton.text = resources.getText(R.string.redeem_button)
-            val success = bundle.getBoolean("success", false)
-            val isExpired = bundle.getBoolean("isExpired", true)
-            val isActivated = bundle.getBoolean("isActivated", false)
-            val isRedeemed = bundle.getBoolean("isRedeemed", false)
-            if (success) {
-                if (!isExpired) {
-                    if (isActivated) {
-                        if (!isRedeemed) {
-                            message1TextView.text = "The following Voucher will be redeemed to get this following promo"
-                            message2TextView.text = bundle.getString("outletPromo", "error")
-                        } else {
-                            message1TextView.text = "The following Voucher is already redeemed here at the following date"
-                            message2TextView.text = bundle.getString("usedDate", "errorerrorerrorerror").substring(0, 10);
-                        }
-                    } else {
-                        message1TextView.text = "The following Voucher is hasn't activated yet. please activate voucher at it's origin outlet"
-                        message2TextView.text = bundle.getString("outletOriginName", "error")
-                    }
-                } else {
-                    message1TextView.text = "The following Voucher is already expired at"
-                    message2TextView.text = bundle.getString("expiryDate", "errorerrorerror").substring(0, 10);
-                }
-            } else {
-                message1TextView.text = "The following Voucher isn't valid"
-                message2TextView.text = ""
-            }
-        } else {
-            GenerateQR().execute(bundle.getString("code", ""))
-            toolbar.setTitle(R.string.activate_title)
-            actionButton.text = resources.getText(R.string.activate_button)
-            val success = bundle.getBoolean("success", false)
-            val isExpired = bundle.getBoolean("isExpired", true)
-            val isActivated = bundle.getBoolean("isActivated", false)
-            val permission = bundle.getBoolean("permission", false)
-
-            if (success) {
-                if (!isExpired) {
-                    if (!isActivated) {
-                        if (permission) {
-                            message1TextView.text = "The following Voucher will be activated and able to be redeemed in other outlet untill it expires on"
-                            message2TextView.text = bundle.getString("expirydate", "error").substring(0, 10);
-                        } else {
-                            message1TextView.text = "The following Voucher is not originated from this outlet, please activate Voucherfrom it's origin Outlet which is"
-                            message2TextView.text = bundle.getString("outletName", "error")
-                        }
-                    } else {
-                        message1TextView.text = "The following Voucher is already active"
-                        message2TextView.text = ""
-                    }
-                } else {
-                    message1TextView.text = "The following Voucher is already expired since"
-                    message2TextView.text = bundle.getString("expiryDate", "error").substring(0, 10);
-                }
-            } else {
-                message1TextView.text = "The following Voucher isn't valid"
-                message2TextView.text = ""
-            }
-
-
-        }
+        val activateRedeem = bundle.getBoolean("activateRedeem")
+        if (activateRedeem)
+            activateLookAndFeel(bundle)
+        else
+            redeemLookAndFeel(bundle)
 
     }
 
+    @SuppressLint("StaticFieldLeak")
     private inner class GenerateQR : AsyncTask<String, Int, Bitmap>() {
         override fun doInBackground(vararg p0: String?): Bitmap? {
             val param = p0[0]
@@ -94,11 +45,125 @@ class RedeemActivity : AppCompatActivity() {
             return bitmap
         }
 
-
         override fun onPostExecute(result: Bitmap?) {
             super.onPostExecute(result)
             actionImageView.setImageBitmap(result)
         }
+    }
 
+    private fun activateLookAndFeel(bundle: Bundle){
+        GenerateQR().execute(bundle.getString("code", ""))
+        toolbar.setTitle(R.string.activate_title)
+        actionButton.text = resources.getText(R.string.activate_button)
+        val success = bundle.getBoolean("success", false)
+        val isExpired = bundle.getBoolean("isExpired", true)
+        val isActivated = bundle.getBoolean("isActivated", false)
+        val permission = bundle.getBoolean("permission", false)
+
+        if (success) {
+            if (!isExpired) {
+                if (!isActivated) {
+                    if (permission) {
+                        message1TextView.text = resources.getString(R.string.activation_confirmation)
+                        message2TextView.text = bundle.getString("expiryDate", "error").substring(0, 10)
+                    } else {
+                        message1TextView.text = resources.getString(R.string.activation_fail_permission)
+                        message2TextView.text = bundle.getString("outletName", "error")
+                    }
+                } else {
+                    message1TextView.text = resources.getString(R.string.activation_fail_active)
+                    message2TextView.text = ""
+                }
+            } else {
+                message1TextView.text = resources.getString(R.string.voucher_expired)
+                message2TextView.text = bundle.getString("expiryDate", "errorerrorerrorerror").substring(0, 10)
+            }
+        } else {
+            message1TextView.text = resources.getString(R.string.voucher_not_found)
+            message2TextView.text = ""
+        }
+        if(success&&!isExpired&&!isActivated&&permission){
+            actionButton.text = "Activate"
+            actionButton.setOnClickListener {
+                BackendAPIRestClient(this.applicationContext).activate(bundle.getString("code"), object : JsonHttpResponseHandler() {
+                    override fun onSuccess(statusCode: Int, headers: Array<out Header>?, response: JSONObject) {
+                        super.onSuccess(statusCode, headers, response)
+
+                        var activateSuccess = false
+                        try {
+                            activateSuccess= response.getBoolean("success")
+                        } catch (e: Exception) {
+                        }
+                        if (activateSuccess){
+                            Toast.makeText(context,"Voucher activation success",Toast.LENGTH_SHORT).show()
+                            finish()
+                        }else
+                            Toast.makeText(context,"Something went wrong",Toast.LENGTH_SHORT).show()
+                    }
+                })
+            }
+        }else{
+            actionButton.text = "Back"
+            actionButton.setOnClickListener {
+                finish()
+            }
+        }
+
+    }
+
+    private fun redeemLookAndFeel(bundle: Bundle){
+        toolbar.setTitle(R.string.redeem_title)
+        actionButton.text = resources.getText(R.string.redeem_button)
+        val success = bundle.getBoolean("success", false)
+        val isExpired = bundle.getBoolean("isExpired", true)
+        val isActivated = bundle.getBoolean("isActivated", false)
+        val isRedeemed = bundle.getBoolean("isRedeemed", false)
+        if (success) {
+            if (!isExpired) {
+                if (isActivated) {
+                    if (!isRedeemed) {
+                        message1TextView.text = resources.getString(R.string.redeem_confirmation)
+                        message2TextView.text = bundle.getString("outletPromo", "error")
+                    } else {
+                        message1TextView.text = resources.getString(R.string.redeem_fail_redeemed)
+                        message2TextView.text = bundle.getString("usedDate", "errorerrorerrorerror").substring(0, 10)
+                    }
+                } else {
+                    message1TextView.text = resources.getString(R.string.redeem_fail_not_activated)
+                    message2TextView.text = resources.getStringArray(R.array.outlet_name)[bundle.getInt("outletOrigin")-1]
+                }
+            } else {
+                message1TextView.text = resources.getString(R.string.voucher_expired)
+                message2TextView.text = bundle.getString("expiryDate", "errorerrorerror").substring(0, 10)
+            }
+        } else {
+            message1TextView.text = resources.getString(R.string.voucher_not_found)
+            message2TextView.text = ""
+        }
+        if(success&&!isExpired&&isActivated&&!isRedeemed){
+            actionButton.text = "Redeem"
+            actionButton.setOnClickListener {
+                BackendAPIRestClient(this.applicationContext).redeem(bundle.getString("code"),bundle.getInt("outletid"), object : JsonHttpResponseHandler() {
+                    override fun onSuccess(statusCode: Int, headers: Array<out Header>?, response: JSONObject) {
+                        super.onSuccess(statusCode, headers, response)
+                        var redeemSuccess = false
+                        try {
+                            redeemSuccess= response.getBoolean("success")
+                        } catch (e: Exception) {
+                        }
+                        if (redeemSuccess){
+                            Toast.makeText(context,"Voucher successfully redeemed",Toast.LENGTH_SHORT).show()
+                            finish()
+                        }else
+                            Toast.makeText(context,"Something went wrong",Toast.LENGTH_SHORT).show()
+                    }
+                })
+            }
+        }else{
+            actionButton.text = "Back"
+            actionButton.setOnClickListener {
+                finish()
+            }
+        }
     }
 }
